837a858426c3c00c1989a2073e3f29d9b93530f1
hilolo <hiloloarena@gmail.com>
2022-06-18 03:34:48 +0100

    klhk

---
+++ src/App.tsx
@@ -0,0 +1,118 @@
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import { useMemo } from "react";
+import {
+    ConnectionProvider,
+    WalletProvider,
+} from "@solana/wallet-adapter-react";
+import * as anchor from "@project-serum/anchor";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+    getPhantomWallet,
+    getSlopeWallet,
+    getSolflareWallet,
+    getSolflareWebWallet,
+    getSolletWallet,
+    getSolletExtensionWallet,
+    getSolongWallet,
+    getLedgerWallet,
+    getSafePalWallet,
+} from "@solana/wallet-adapter-wallets";
+
+import {
+    WalletModalProvider
+} from '@solana/wallet-adapter-react-ui';
+
+import "./App.css";
+import Home from "./Home";
+
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+
+const candyMachineId = new anchor.web3.PublicKey(
+  process.env.REACT_APP_CANDY_MACHINE_ID!
+);
+
+const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
+
+const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
+const connection = new anchor.web3.Connection(rpcHost);
+
+const txTimeout = 30000; // milliseconds (confirm this works for your project)
+
+const ThemeColors = {
+  primary: "white",
+  blue: "blue",
+  red: "red",
+  purple: "purple",
+  orange: "orange",
+  green: "green"
+
+};
+const theme = createTheme({
+    palette: {
+        type: 'dark',
+    },
+    overrides: {
+        MuiButtonBase: {
+            root: {
+                justifyContent: 'flex-start',
+            },
+        },
+        MuiButton: {
+            root: {
+                padding: '20px 30px',
+                color:ThemeColors.primary
+
+            },
+            startIcon: {
+                marginRight: 8,
+            },
+            endIcon: {
+                marginLeft: 8,
+            },
+        },
+    },
+});
+
+const App = () => {
+    // Custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), []);
+
+    // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+    // Only the wallets you configure here will be compiled into your application, and only the dependencies
+    // of wallets that your users connect to will be loaded.
+    const wallets = useMemo(
+        () => [
+            getPhantomWallet(),
+            getSlopeWallet(),
+            getSolflareWallet(),
+            getSolflareWebWallet(),
+            getSolletWallet({ network }),
+            getSolletExtensionWallet({ network }),
+            getSolongWallet(),
+            getLedgerWallet(),
+            getSafePalWallet(),
+        ],
+        []
+    );
+
+  return (
+      <ThemeProvider theme={theme}>
+        <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect={true}>
+            <WalletModalProvider>
+              <Home
+                candyMachineId={candyMachineId}
+                connection={connection}
+                txTimeout={txTimeout}
+                rpcHost={rpcHost}
+              />
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      </ThemeProvider>
+  );
+};
+
+export default App;
